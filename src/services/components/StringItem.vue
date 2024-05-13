3fd8606c7d545a73bd1561fc2926cee73117e033
Daniil <qwer.82@mail.ru>
2024-05-13 19:00:23 +0500

    Upd base-style.scss, PInput and PSelect

--- src/services/components/StringItem.vue
+++ src/services/components/StringItem.vue
@@ -42,11 +42,19 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/styles/base-style.scss';
+
 .string {
   max-width: 100%;
   width: 100%;
   display: flex;
   align-items: center;
   background: inherit;
+  font-family: $base-font;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+  color: $base-font-color;
+  font-size: $base-font-size;
 }
 </style>
