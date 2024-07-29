be3e309216e784bf1a947337f09fec49318b0553
LakkinziMusic <lakkinzimusic@gmail.com>
2024-07-29 10:30:02 +0300

    Clear

--- src/views/adminLayout/menuList.ts
+++ src/views/adminLayout/menuList.ts
@@ -3,8 +3,15 @@ import IAdminMenu from '@/services/interfaces/IAdminMenu';
 const menuList: IAdminMenu[] = [
   {
     name: 'Исследования',
-    to: '/admin/researches',
+    to: '',
     icon: 'el-icon-info',
+    children: [
+      {
+        name: 'Список',
+        to: '/admin/researches',
+        icon: 'el-icon-info',
+      },
+    ],
   },
 ];
 
