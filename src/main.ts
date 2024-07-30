bbd74631135258e9307a285d93702c103f03429b
Daniil <qwer.82@mail.ru>
2024-07-30 18:45:05 +0500

    Upd PButton style

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router/index';
 import '@/router/componentHooks';
+import '@/services/styles/index.css';
 
 const app = createApp(App);
 app.use(router);
