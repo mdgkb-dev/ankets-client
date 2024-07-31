fceed2673f2dd54eea8d61d69881ee53e7d16c38
LakkinziMusic <lakkinzimusic@gmail.com>
2024-08-01 00:45:34 +0300

    ResearchToForm

---
+++ src/services/components/Atoms/PCheckbox/PCheckBox.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="check-line">
+    <label class="check-field__label" :style="{ fontSize: fontSize }">
+      <input v-model="switchPosition" class="check-field__input" type="checkbox" />
+      <div class="pointer">
+        <slot />
+      </div>
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const switchPosition = defineModel<boolean>();
+const emits = defineEmits(['switchPosition']);
+defineProps({
+  label: { type: String, default: 'label' },
+  fontSize: { type: String, default: '14px' },
+});
+emits('switchPosition', switchPosition);
+</script>
+
+<style lang="scss" scoped>
+@import '@/services/assets/styles/index.scss';
+
+.hidden {
+  display: none;
+}
+
+.pointer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  cursor: pointer;
+  height: auto;
+}
+
+.check-line {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  max-width: 560px;
+  width: 100%;
+  display: flex;
+  justify-content: left;
+  margin: 0 auto;
+}
+
+.check-field__input {
+  display: none;
+}
+
+.check-field__label {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  font-size: 12px;
+  font-family: Gilroy, Arial, Helvetica, sans-serif;
+  color: #5f6a99;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
+</style>
---
+++ src/services/components/Atoms/PSelect/PSelect.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="text-field" :style="{ margin: margin, padding: padding }">
+    <label v-if="label" class="text-field__label" :for="label">
+      {{ label }}
+    </label>
+    <div class="field">
+      <div class="left-field">
+        <slot name="left" />
+      </div>
+      <div class="sl" @mouseover="showClearIcon = true" @mouseleave="showClearIcon = false">
+        <div v-if="clearable" v-show="showClearIcon" class="clear" @click="clear">
+          <IconClose margin="3px 0 0 0" />
+        </div>
+        <select v-model="model" class="text-field__input" @change="select">
+          <option selected hidden>{{ placeholder }}</option>
+          <slot />
+        </select>
+      </div>
+      <div class="right-field">
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconClose from '@/services/components/Icons/IconClose.vue';
+
+const model: unknown = defineModel<unknown>();
+const ph: Ref<boolean> = ref(true);
+const emits = defineEmits(['change', 'clear']);
+defineOptions({ inheritAttrs: false });
+
+const showClearIcon = ref(false);
+
+defineProps({
+  text: { type: String, default: '', required: false },
+  label: { type: String, default: '', required: false },
+  placeholder: { type: String, default: '', required: false },
+  value: { type: String, default: '', required: false },
+  readonly: { type: Boolean, default: false, required: false },
+  disabled: { type: Boolean, default: false, required: false },
+  margin: { type: String, required: false, default: '' },
+  padding: { type: String, required: false, default: '' },
+  clearable: { type: Boolean, required: false, default: false },
+});
+
+const select = (v: unknown) => {
+  ph.value = false;
+  emits('change', v);
+};
+const clear = () => {
+  emits('clear', undefined);
+  emits('change', undefined);
+  ph.value = true;
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/services/assets/styles/index.scss';
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+option {
+  padding: 0;
+}
+
+.field {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  background: $input-background;
+  border-radius: $p-input-border-radius;
+  border: $p-input-border;
+  padding: $p-input-padding;
+  margin: $p-input-margin;
+  overflow: hidden;
+}
+
+.sl {
+  width: 100%;
+  position: relative;
+}
+
+.right-field {
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  width: auto;
+}
+
+.left-field {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: auto;
+}
+
+.text-field {
+  width: $input-width;
+}
+
+.text-field__label {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: 100%;
+  font-family: $input-font;
+  color: $input-label-color;
+  font-size: $base-font-size;
+  margin: $input-label-margin;
+}
+
+.text-field__input {
+  width: 100%;
+  height: $input-height;
+  font-family: $input-font;
+  font-size: $input-font-size;
+  background: $input-background;
+  color: $input-font-color;
+  border: $input-border;
+  margin: $input-margin;
+  padding: $input-padding;
+  padding: 0;
+  cursor: pointer;
+}
+
+.ph {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 5px;
+  z-index: 0;
+  font-family: $input-font;
+  font-size: $input-font-size;
+  color: $input-font-color;
+  white-space: nowrap;
+  pointer-events: none;
+  cursor: pointer;
+}
+
+.text-field__input:focus {
+  color: $input-font-color;
+  outline: 0;
+}
+
+.text-field__input::-ms-expand {
+  display: none;
+}
+
+.text-field__input:hover {
+  border-color: #888;
+}
+
+.text-field__input option {
+  font-weight: normal;
+}
+
+.clear {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: -6px;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: $input-background;
+  transition: 0;
+  cursor: pointer;
+}
+</style>
